4f13604d31e7b8a02a777d1df0a3f84728e27563
Milad Faridnia <miladfaridnia@gmail.com>
2023-11-16 14:37:03 +0330

    Feat: Complete OnboardingScreen ui

--- app/src/main/java/com/faridnia/metime/presentation/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/faridnia/metime/presentation/ui/onboarding/OnboardingScreen.kt
@@ -1,31 +1,47 @@
 package com.faridnia.metime.presentation.ui.onboarding
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.faridnia.metime.R
+import com.faridnia.metime.presentation.ui.theme.MeTimeTheme
 import com.faridnia.metime.util.LightAndDarkPreview
 
 @LightAndDarkPreview
 @Composable
 fun PreviewOnboardingScreen() {
-    OnboardingScreen(
-        state = remember { mutableStateOf(OnboardingState()) },
-        onEvent = {},
-        navController = rememberNavController()
-    )
+    MeTimeTheme {
+        OnboardingScreen(
+            state = remember { mutableStateOf(OnboardingState()) },
+            onEvent = {},
+            navController = rememberNavController()
+        )
+    }
 }
 
 @Composable
@@ -42,9 +58,54 @@ fun OnboardingScreen(
             .background(color = MaterialTheme.colorScheme.background)
             .padding(start = 24.dp, end = 24.dp),
     ) {
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
+        Text(
+            text = "MeTime",
+            style = TextStyle(
+                fontSize = 19.sp,
+                fontFamily = FontFamily(Font(R.font.raleway_light)),
+                fontWeight = FontWeight(700),
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center,
+            )
+        )
 
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(385.dp),
+            painter = painterResource(id = R.drawable.onboarding),
+            contentDescription = "image description",
+            contentScale = ContentScale.Fit
+        )
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Text(
+            text = "Welcome to \nThe Gallery Salon!",
+            style = TextStyle(
+                fontSize = 32.sp,
+                fontFamily = FontFamily(Font(R.font.raleway_light)),
+                fontWeight = FontWeight(700),
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center,
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Follow the steps to schedule your next appointment with us.",
+            style = TextStyle(
+                fontSize = 18.sp,
+                lineHeight = 22.48.sp,
+                fontFamily = FontFamily(Font(R.font.raleway_light)),
+                fontWeight = FontWeight(500),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                textAlign = TextAlign.Center,
+            )
+        )
 
     }
 }
