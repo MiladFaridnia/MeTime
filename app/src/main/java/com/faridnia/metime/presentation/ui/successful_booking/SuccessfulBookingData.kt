bd7782aa0e923a784088abfed5e75b9507b1a44e
Milad Faridnia <miladfaridnia@gmail.com>
2024-01-20 12:21:44 +0330

    Ref: Change SuccessfulBookingData service list to service type list

--- app/src/main/java/com/faridnia/metime/presentation/ui/successful_booking/SuccessfulBookingData.kt
+++ app/src/main/java/com/faridnia/metime/presentation/ui/successful_booking/SuccessfulBookingData.kt
@@ -1,14 +1,35 @@
 package com.faridnia.metime.presentation.ui.successful_booking
 
-import com.faridnia.metime.presentation.ui.choose_service.Service
+import com.faridnia.metime.presentation.ui.choose_service_type.ServiceType
+import com.faridnia.metime.presentation.ui.choose_service_type.getSampleServiceTypes
 import com.faridnia.metime.presentation.ui.professionals_calendar.ProfessionalData
+import com.faridnia.metime.presentation.ui.professionals_calendar.getSampleProfessionalData
+import com.faridnia.metime.util.getCurrentDateTime
 import java.util.Date
 
 data class SuccessfulBookingData(
     val salonName: String,
     val address: String,
     val bookDate: Date,
-    val professionalData : ProfessionalData,
-    val serviceList : List<Service>,
-    val price : Int
+    val professionalData: ProfessionalData,
+    val serviceTypeList: List<ServiceType>,
+    val price: Int
 )
+
+fun getSampleSuccessfulBookingData(): List<SuccessfulBookingData> {
+    val successfulBookingList = mutableListOf<SuccessfulBookingData>()
+
+    for (i in 0..10) {
+        successfulBookingList.add(
+            SuccessfulBookingData(
+                salonName = "Gallery",
+                address = "Address",
+                bookDate = getCurrentDateTime(),
+                price = 33,
+                professionalData = getSampleProfessionalData(),
+                serviceTypeList = getSampleServiceTypes()
+            )
+        )
+    }
+    return successfulBookingList
+}
