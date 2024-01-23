37a3aae46e3b7aee9c6105c74b0ea36d36e4f256
Milad Faridnia <miladfaridnia@gmail.com>
2024-01-23 18:31:01 +0330

    Feat: Add Gradle dependencies to buildSrc

---
+++ buildSrc/src/main/java/DaggerHilt.kt
@@ -0,0 +1,5 @@
+object DaggerHilt {
+    const val version = "2.48.1"
+    const val hiltAndroid = "com.google.dagger:hilt-android:$version"
+    const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
+}
---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,14 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+//    const val coreUi = ":core-ui"
+
+    const val onboardingData = ":onboarding:onboarding_data"
+    const val onboardingDomain = ":onboarding:onboarding_domain"
+    const val onboardingPresentation = ":onboarding:onboarding_presentation"
+
+    const val trackerData = ":booking:booking_data"
+    const val trackerDomain = ":booking:booking_domain"
+    const val trackerPresentation = ":booking:booking_presentation"
+}
