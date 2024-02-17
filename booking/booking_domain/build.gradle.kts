c1780890d81964f52540a59e19c57c0606aa0061
Milad Faridnia <miladfaridnia@gmail.com>
2024-02-17 18:18:39 +0330

    Fix: cleanup gradle and dependencies

--- booking/booking_domain/build.gradle.kts
+++ booking/booking_domain/build.gradle.kts
@@ -33,10 +33,9 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(AndroidX.coreKtx/*"androidx.core:core-ktx:1.12.0"*/)
+    implementation(AndroidX.appCompat/*"androidx.appcompat:appcompat:1.6.1"*/)
+    testImplementation(Testing.junit4)
+    androidTestImplementation(Testing.junitAndroidExt)
+    androidTestImplementation(Testing.espresso)
 }
