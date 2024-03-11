2e5b1794fc305f4b7128a7d4bb9606288037b17e
Milad Faridnia <miladfaridnia@gmail.com>
2024-03-11 14:04:59 +0330

    Feat: Add Firebase analytics dependencies

--- buildSrc/src/main/java/Google.kt
+++ buildSrc/src/main/java/Google.kt
@@ -1,4 +1,11 @@
 object Google {
     private const val materialVersion = "1.4.0"
     const val material = "com.google.android.material:material:$materialVersion"
+
+
+    private const val FIREBASE_BOM = "32.7.4"
+    const val fireBaseBom = "com.google.firebase:firebase-bom:$FIREBASE_BOM"
+    const val firebaseAnalytics = "com.google.firebase:firebase-analytics"
+
+
 }
