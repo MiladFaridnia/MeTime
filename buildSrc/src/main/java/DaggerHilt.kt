fea88408dfd825ce5b7b61be181d6780f1122c8f
Milad Faridnia <miladfaridnia@gmail.com>
2024-05-21 11:04:33 +0330

    Chore: Update package gradle and downgrade hilt version

--- buildSrc/src/main/java/DaggerHilt.kt
+++ buildSrc/src/main/java/DaggerHilt.kt
@@ -1,6 +1,6 @@
 object DaggerHilt {
-    const val version = "2.49"
-    const val androidXVersion = "1.2.0"
+    const val version = "2.48.1"
+    const val androidXVersion = "1.1.0"
     const val hiltAndroid = "com.google.dagger:hilt-android:$version"
     const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$version"
     const val androidXHiltCompiler = "androidx.hilt:hilt-compiler:$androidXVersion"
